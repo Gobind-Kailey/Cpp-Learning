424640856e30105fb66598260e04c6791c075b79
kaileg2 <gobind2975@gmail.com>
2025-06-22 19:38:58 -0400

    Pushing DMA lesson

---
+++ Section 12 Pointers and References/DMA.cpp
@@ -0,0 +1,23 @@
+// Dynamic Memory Allocation 
+
+
+#inlucde <iostream> 
+
+using namespace std; 
+
+int main()
+{
+
+    int *int_ptr = nullptr; 
+    int_ptr = new int; 
+    
+    cout << int_ptr <, endl; 
+    delete int_ptr; 
+
+    size_t size = 0; 
+    
+
+
+
+    return 0; 
+}
